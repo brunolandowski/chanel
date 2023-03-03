92ee71f4931f5494cfff2234124aaef5bf83e319
Bruno Landowski <bruno.landowski@gmail.com>
2023-03-03 16:54:33 +0100

    websocket

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-// Import packages
-const express = require("express");
-const socketIO = require("socket.io");
-const path = require("path");
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:8080');
 
-// Configuration
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+// Connection opened
+socket.addEventListener('open', (event) => {
+    socket.send('Hello Server!');
+});
 
-// Start server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
- .listen(PORT, () => console.log("Listening on localhost:" + PORT));
-
-// Initiatlize SocketIO
-const io = socketIO(server);
-
-// Register "connection" events to the WebSocket
-io.on("connection", function(socket) {
-  // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room)
-    // Register "image" events, sent by the client
-    socket.on("image", function(msg) {
-      // Broadcast the "image" event to all other clients in the room
-      socket.broadcast.to(room).emit("image", msg);
-    });
-  })
+// Listen for messages
+socket.addEventListener('message', (event) => {
+    console.log('Message from server ', event.data);
 });
